19b7f464b4ea182775d94a26c2582b68bd623348
JP9501 <962950831@qq.com>
2017-11-17 16:02:23 +0800

    vue脚本架改进

---
+++ src/component/App.vue
@@ -0,0 +1,22 @@
+<template>
+<main>
+<p>{{ msg }}</p>
+</main>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            msg:'脚手架初搭建'
+        }
+    }
+}
+
+</script>
+<style scoped>
+    
+    p{
+    font-size:30px;
+    color: #0a0;
+    }
+</style>
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+
+import Vue from 'Vue';
+import App from '../component/App.vue';
+
+
+new Vue({
+   el:'#app',
+   render:function(c){
+        return c(App)
+   } 
+})
